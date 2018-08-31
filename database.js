1842a66500fe1454c3ced6b65a66283a8ef85759
zhuohai <1181316781@qq.com>
2018-08-31 18:28:38 +0800

    新增

---
+++ database.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql'); //调用MySQL模块
+
+//创建一个connection
+var connection = mysql.createConnection({
+  host: 'localhost', //主机
+  user: 'root', //MySQL认证用户名
+  password: 'zh12345678', //MySQL认证用户密码
+  port: '3306', 
+  database: "merit"//端口号
+});
+//创建一个connection
+connection.connect(function (err) {
+  if (err) {
+    console.log('[query] - :' + err);
+    return;
+  }
+  console.log('[connection connect] succeed!');
+});
